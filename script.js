5f6d1b98f8b26445cd5606be3a17475f5a64ef4a
Luiz Eduardo <duduamaralspereira@gmail.com>
2024-09-28 19:42:54 -0300

    desenvolvendo a interação com o a logo

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const imgs = document.getElementById("img");  
-const img = document.querySelectorAll("#img img");  
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
 const inputLogo = document.getElementById("logoInput");
 const logo = document.getElementById("logo");
 const logoContainer = document.getElementById("logoContainer");
 
 let idx = 0;
+var x = 0; var y = 0;
+
 
 function carossel() {
     idx++;
@@ -33,11 +35,11 @@ function proximaImg() {
 }
 
 document.getElementById("anterior").addEventListener("click", () => {
-    anteriorImg();  
+    anteriorImg();
 });
 
 document.getElementById("proxima").addEventListener("click", () => {
-    proximaImg();  
+    proximaImg();
 });
 
 inputLogo.addEventListener("change", (event) => {
@@ -45,10 +47,81 @@ inputLogo.addEventListener("change", (event) => {
 
     if (file) {
         const reader = new FileReader();
-        reader.onload = function(e) {
+        reader.onload = function (e) {
             logo.src = e.target.result;
-            logo.classList.remove("hidden"); 
+            logo.classList.remove("hidden");
         };
         reader.readAsDataURL(file);
     }
 });
+
+interact("#logo")
+  .draggable({
+    modifiers: [
+      interact.modifiers.snap({
+        targets: [
+          interact.snappers.grid({ x: 30, y: 30 })
+        ],
+        range: Infinity,
+        relativePoints: [ { x: 0, y: 0 } ]
+      }),
+      interact.modifiers.restrict({
+        restriction: "parent",
+        elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
+        endOnly: true
+      })
+    ],
+    inertia: true
+  })
+  .on('dragmove', function (event) {
+    x += event.dx
+    y += event.dy
+
+    event.target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+  })
+
+  interact('#logo')
+  .resizable({
+    edges: { left: true, right: true, bottom: true, top: true },
+
+    listeners: {
+      move (event) {
+        var target = event.target
+        var x = (parseFloat(target.getAttribute('data-x')) || 0)
+        var y = (parseFloat(target.getAttribute('data-y')) || 0)
+
+        target.style.width = event.rect.width + 'px'
+        target.style.height = event.rect.height + 'px'
+
+        x += event.deltaRect.left
+        y += event.deltaRect.top
+
+        target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
+
+        target.setAttribute('data-x', x)
+        target.setAttribute('data-y', y)
+        target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height)
+      }
+    },
+    modifiers: [
+      interact.modifiers.restrictEdges({
+        outer: 'parent'
+      }),
+
+      interact.modifiers.restrictSize({
+        min: { width: 100, height: 50 }
+      })
+    ],
+
+    inertia: true
+  })
+  .draggable({
+    listeners: { move: window.dragMoveListener },
+    inertia: true,
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ]
+  })
